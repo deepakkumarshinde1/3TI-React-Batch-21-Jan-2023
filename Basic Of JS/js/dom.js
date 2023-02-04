d9f5146bda39dbe5fac581011bcbb610c2f73e99
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2023-02-04 12:14:08 +0530

    DOM-day-2

--- Basic Of JS/js/dom.js
+++ Basic Of JS/js/dom.js
@@ -7,11 +7,41 @@
 
 /** what is event ?
  *  action perform by user
- *      click, dblClick, mouseover, drag, drop, croll
+ *      click, dblClick, mouseover, drag, drop, scroll
  *      keypress, keyup,
  *      load, unload,
  */
-function clickBtn1() {
-  console.log("click");
-}
+/*
 console.log(document);
+
+let btnId = document.getElementById("btn-id"); // => single element
+let button = document.getElementsByTagName("button"); // [] => element
+let btnClass = document.getElementsByClassName("btn-class"); // []=> element
+
+let btnClassSelector = document.querySelector(".btn-class"); // 1st single
+let btnClassSelectorAll = document.querySelectorAll(".btn-class"); // []=> element
+
+console.log(btnId);
+console.log(button);
+console.log(btnClass);
+console.log(btnClassSelector);
+console.log(btnClassSelectorAll);
+*/
+// .btn-class , #btn-id , button
+
+function getElement(element) {
+  let _element = document.querySelector(element);
+  if (_element === null) {
+    throw new Error(element + " is not available in HTML");
+  } else {
+    return _element;
+  }
+}
+
+var btnId = getElement("#btn-id");
+// if you dont get value , you will get a null
+
+// event add
+btnId.addEventListener("click", function () {
+  console.log("click");
+});
